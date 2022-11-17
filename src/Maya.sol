c9a4e10ebc947b26e83c9e626fef910e18a18d3d
coteriecapital <peter@coterie.capital>
2022-11-17 01:03:03 -0500

    Added opensea blacklist

--- src/Maya.sol
+++ src/Maya.sol
@@ -1,11 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "openzeppelin-contracts/access/Ownable.sol";
+import "openzeppelin-contracts/utils/cryptography/MerkleProof.sol";
+import "openzeppelin-contracts/utils/Strings.sol";
+import "openzeppelin-contracts/token/ERC1155/ERC1155.sol";
+import "operator-filter-registry/DefaultOperatorFilterer.sol";
 
-contract Maya is ERC1155, Ownable {
+contract Maya is ERC1155, DefaultOperatorFilterer, Ownable {
     uint256 public numTokens = 0;
     string public name = "Maya";
     string public symbol = "MAYA";
@@ -368,6 +369,34 @@ contract Maya is ERC1155, Ownable {
 
     function uri(uint256 tokenId) public view override returns (string memory) {
         return tokens[tokenId].uri;
+    }
+
+    function setApprovalForAll(address operator, bool approved)
+        public
+        override
+        onlyAllowedOperatorApproval(operator)
+    {
+        super.setApprovalForAll(operator, approved);
+    }
+
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 amount,
+        bytes memory data
+    ) public override onlyAllowedOperator(from) {
+        super.safeTransferFrom(from, to, tokenId, amount, data);
+    }
+
+    function safeBatchTransferFrom(
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) public virtual override onlyAllowedOperator(from) {
+        super.safeBatchTransferFrom(from, to, ids, amounts, data);
     }
 
     function _beforeTokenTransfer(
